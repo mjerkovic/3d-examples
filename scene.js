c9a95798c6c99ce05a39f1d3883626e98834ae15
Mark Jerkovic <mjerkovic@aconex.com>
2013-05-06 22:22:21 +1000

    Add more physics effects.

--- scene.js
+++ scene.js
@@ -1,16 +1,19 @@
-function initScene() {
+function initScene(camera) {
     var scene = createScene();
     addLightsTo(scene);
     addGroundTo(scene);
-    scene.add(compass());
+    var compass = this.compass();
+    camera.lookAt(compass.position);
+    scene.add(compass);
     scene.add(building());
     scene.add(bouncingBall());
+    crashing(scene);
 
     return scene;
 }
 function createScene() {
     var scene = new Physijs.Scene;
-    scene.setGravity(new THREE.Vector3(0, -9.82, 0));
+    scene.setGravity(new THREE.Vector3(0, -30, 0));
     scene.addEventListener(
         'update',
         function () {
@@ -21,6 +24,7 @@ function createScene() {
     //scene.fog = new THREE.Fog(0xEEEEEE, 1, 100);
     return scene;
 }
+
 function bouncingBall() {
     var bouncingBallGeometry = new THREE.SphereGeometry(2, 32, 32);
     var bouncingMaterial = new Physijs.createMaterial(
@@ -28,12 +32,39 @@ function bouncingBall() {
             specular: 0x20205F, shininess: 90 }), .8, 1);
     var bouncingBall = new Physijs.SphereMesh(bouncingBallGeometry, bouncingMaterial, 1);
 //				bouncingMesh.position.set(50, 30, -10);
-    bouncingBall.position.set(0, 100, 0);
+    bouncingBall.position.set(0.2, 100, 0);
     bouncingBall.addEventListener("collision", function(other, a, b) {
         console.log("linearVelocity = " + a.toArray().join());
         console.log("angularVelocity = " + b.toArray().join());
     });
     return bouncingBall;
+}
+
+function crashing(scene) {
+    var bouncingBallGeometry = new THREE.SphereGeometry(2, 32, 32);
+    var bouncingMaterial = new Physijs.createMaterial(
+        new THREE.MeshPhongMaterial({ color: 0x25256F,
+            specular: 0x20205F, shininess: 90 }), .8, 0.4);
+    var crash1 = new Physijs.SphereMesh(bouncingBallGeometry, bouncingMaterial, 1);
+    crash1.position.set(50, 2, -3);
+    crash1.velocity = 0.2;
+    crash1.addEventListener("collision", function(other, linear, angular) {
+        crash1.velocity = 0;
+    });
+    var crash2 = new Physijs.SphereMesh(bouncingBallGeometry, bouncingMaterial, 1);
+    crash2.position.set(100, 2, -3);
+    crash2.addEventListener("collision", function(other, linear, angular) {
+        crash2.velocity = 0;
+    });
+    crash2.velocity = 0.2;
+    scene.doIt = function() {
+        crash1.position.x += crash1.velocity;
+        crash1.__dirtyPosition = crash1.velocity > 0;
+        crash2.position.x -= crash2.velocity;
+        crash2._dirtyPosition = crash2.velocity = true;
+    };
+    scene.add(crash1);
+    scene.add(crash2);
 }
 
 function addGroundTo(scene) {
@@ -88,20 +119,19 @@ function compass() {
     //	specular: 0x20205F, shininess: 90 });
     var globeMaterial = new Physijs.createMaterial(
         new THREE.MeshPhongMaterial({ color: 0x25256F,
-            specular: 0x20205F, shininess: 90 }), .8, 0.1);
-    //material.ambient.setRGB(material.color.r*0.4, material.color.g*0.4,
-    //	material.color.b*0.4);
+            specular: 0x20205F, shininess: 90 }), 0.8, 0.1);
     var globe = new Physijs.SphereMesh(globeGeometry, globeMaterial, 1);
     globe.position.set(0, 10, 0);
 
     var barGeometry = new THREE.CylinderGeometry(0.5, 0.5, 10, 64, 64);
     var pointGeometry = new THREE.CylinderGeometry(0, 1, 1.5, 32, 32);
 
-    var yBarGeometryMaterial = new THREE.MeshLambertMaterial({ color: 0x33CC00 });
-    var m = new THREE.Mesh(barGeometry, yBarGeometryMaterial);
+    var yBarGeometryMaterial = new Physijs.createMaterial(
+        new THREE.MeshLambertMaterial({ color: 0x33CC00 }), 0.8, 0.6);
+    var m = new Physijs.CylinderMesh(barGeometry, yBarGeometryMaterial, 1);
     m.position.y = 5;
     //scene.add(m);
-    var yPointMesh = new THREE.Mesh(pointGeometry, yBarGeometryMaterial);
+    var yPointMesh = new Physijs.CylinderMesh(pointGeometry, yBarGeometryMaterial, 1);
     yPointMesh.position.y = 10;
     //scene.add(yPointMesh);
 
@@ -110,12 +140,13 @@ function compass() {
     yPointer.add(yPointMesh);
     //scene.add(yPointer);
 
-    var xBarGeometryMaterial = new THREE.MeshLambertMaterial({ color: 0xCC0000 });
-    var m2 = new THREE.Mesh(barGeometry, xBarGeometryMaterial);
+    var xBarGeometryMaterial = new Physijs.createMaterial(
+        new THREE.MeshLambertMaterial({ color: 0xCC0000 }), 0.8, 0.6);
+    var m2 = new Physijs.CylinderMesh(barGeometry, xBarGeometryMaterial, 1);
     m2.rotation.z = 90 * (Math.PI/180);
     m2.position.x = 5;
     //scene.add(m2);
-    var xPointMesh = new THREE.Mesh(pointGeometry, xBarGeometryMaterial);
+    var xPointMesh = new Physijs.CylinderMesh(pointGeometry, xBarGeometryMaterial, 1);
     xPointMesh.rotation.z = -90 * (Math.PI/180);
     xPointMesh.position.x = 10;
     //scene.add(xPointMesh);
@@ -125,12 +156,13 @@ function compass() {
     xPointer.add(xPointMesh);
     //scene.add(xPointer);
 
-    var zBarGeometryMaterial = new THREE.MeshLambertMaterial({ color: 0x0033CC });
-    var m3 = new THREE.Mesh(barGeometry, zBarGeometryMaterial);
+    var zBarGeometryMaterial = new Physijs.createMaterial(
+        new THREE.MeshLambertMaterial({ color: 0x0033CC }), 0.8, 0.6);
+    var m3 = new Physijs.CylinderMesh(barGeometry, zBarGeometryMaterial, 1);
     m3.rotation.x = 90 * (Math.PI/180);
     m3.position.z = 5;
     //scene.add(m3);
-    var zPointMesh = new THREE.Mesh(pointGeometry, zBarGeometryMaterial);
+    var zPointMesh = new Physijs.CylinderMesh(pointGeometry, zBarGeometryMaterial, 1);
     zPointMesh.rotation.x = 90 * (Math.PI/180);
     zPointMesh.position.z = 10;
     //scene.add(zPointMesh);
